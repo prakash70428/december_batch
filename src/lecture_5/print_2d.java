1c1877d247f12f91916ce32a1e9c2d79e34e230c
prakash70428 <prakashmendish@gmail.com>
2024-01-23 00:03:45 +0530

    till ringRotate

---
+++ src/lecture_5/print_2d.java
@@ -0,0 +1,28 @@
+package lecture_5;
+import java.util.*;
+public class print_2d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        
+        int[][] arr = new int[n][m];
+        for(int i=0 ; i < n;i++) {
+        	for(int j=0 ; j < m;j++) {
+        		arr[i][j] = scn.nextInt();
+        	}
+        }
+        
+        for(int i=0 ; i < n;i++) {
+        	for(int j=0 ; j < m;j++) {
+        		System.out.print(arr[i][j] + " ");
+        	}
+        	System.out.println();
+        }
+        
+        
+	}
+
+}
